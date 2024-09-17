12efa6c22b6299e952c56fcf5e8df25bbf711044
pbalazs22 <120101312+pbalazs22@users.noreply.github.com>
2024-09-17 08:44:45 +0200

    Add files via upload

---
+++ veletlenszam/veletlenszam/Program.cs
@@ -0,0 +1,8 @@
+﻿Random veletlen = new Random();
+int veletlenSzam;
+for ( int i = 1; i < 7; i++ )
+    {
+    veletlenSzam = veletlen.Next(61);
+    Console.Write( "{0}",veletlenSzam );
+    }
+Console.WriteLine();
