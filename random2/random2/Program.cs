e930007d917bf6318ec6a38f7b34bb8e09f656a5
pbalazs22 <120101312+pbalazs22@users.noreply.github.com>
2024-09-17 08:45:42 +0200

    Add files via upload

---
+++ random2/random2/Program.cs
@@ -0,0 +1,9 @@
+﻿Random veletlen = new Random();
+int a = veletlen.Next(6) + 1;
+int b = veletlen.Next(6) + 1;
+if (a > b)
+    Console.WriteLine("{0} > {1}", a, b);
+else if (b > a) 
+    Console.WriteLine("{0} < {1}", a, b);
+else
+    Console.WriteLine("{0} = {1}", a, b);
