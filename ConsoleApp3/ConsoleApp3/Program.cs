43275e96459f60a37dd9956808c36df5f18a1430
pbalazs22 <120101312+pbalazs22@users.noreply.github.com>
2024-10-08 11:32:32 +0200

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Adj meg egy számot:");
+int val = int.Parse(Console.ReadLine());
+int i, j;
+// i sor j a csillag
+for (i = 1; i <= val; i++)
+{
+    for (j = 0; j <= val - i; j++) // csillagok szama nő
+        Console.Write("*");
+    Console.WriteLine("");
+}
+
+
+
+
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,8 @@
+﻿string szoveg = "", forditott = "";
+int i = 0;
+Console.WriteLine("Szöveg: ");
+szoveg = Console.ReadLine();
+for (i = szoveg.Length - 1; i >= 0; i--)
+    forditott = forditott + szoveg[i];
+Console.WriteLine("Visszafelé:");
+Console.WriteLine(forditott.ToLower());
