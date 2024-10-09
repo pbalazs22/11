e6e4beda8a49ac6073cd00d93413c6a567b7c6e9
pbalazs22 <120101312+pbalazs22@users.noreply.github.com>
2024-10-09 14:15:49 +0200

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] egeszek = new int[30];
+Random rnd = new Random();
+for (int i = 0; i < egeszek.Length; i++)
+    egeszek[i] = rnd.Next(0, 100);
+
+foreach (int item in egeszek)
+
+    Console.WriteLine("{0}", item);
+
+Console.WriteLine();
+Console.WriteLine("1. elem: {0}", egeszek[0]);
+Console.WriteLine("15.elem: {0}", egeszek[14]);
+Console.WriteLine("utolso elem: {0}" egeszek[egeszek.Length - 1]);
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,18 @@
+﻿int elemszam = 100;
+int fej = 0;
+int iras  = 0;
+int[] veletlenszamok = new int[elemszam];
+Random elem = new Random();
+for (int i = 0; i < veletlenszamok.Length; i++);
+
+    veletlenszamok[i] = elem.Next(0, 2);
+
+foreach (int tombelem in veletlenszamok)
+
+    if (tombelem == 0)
+        Console.WriteLine("fej", tombelem);
+    else
+        Console.WriteLine("írás", tombelem);
+
+Console.WriteLine("fej: {0}", fej);
+Console.WriteLine("írás: {0}", iras);
